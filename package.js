3eb325e7594b56c08a2faa6d3922b95bb5f66a5d
leblanmonster <73737394+leblanmonster@users.noreply.github.com>
2021-10-14 17:22:20 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,51 @@
+{
+  "_from": "sudokuresolver@2.0.1",
+  "_id": "sudokuresolver@2.0.1",
+  "_inBundle": false,
+  "_integrity": "sha512-JnOgKsZYL5mogxe9qi9tikfXHWP0D9J+EtKK0zhLoopUx4fnG07KQWNLSqcIte8gckTUZumG9GkIm7pZhsDQYQ==",
+  "_location": "/sudokuresolver",
+  "_phantomChildren": {},
+  "_requested": {
+    "type": "version",
+    "registry": true,
+    "raw": "sudokuresolver@2.0.1",
+    "name": "sudokuresolver",
+    "escapedName": "sudokuresolver",
+    "rawSpec": "2.0.1",
+    "saveSpec": null,
+    "fetchSpec": "2.0.1"
+  },
+  "_requiredBy": [
+    "#USER",
+    "/"
+  ],
+  "_resolved": "https://registry.npmjs.org/sudokuresolver/-/sudokuresolver-2.0.1.tgz",
+  "_shasum": "7adf42613340073d242ead023614c8d50f08722a",
+  "_spec": "sudokuresolver@2.0.1",
+  "_where": "/private/var/mobile/Containers/Data/Application/5CF522D9-60EE-42A3-B0E1-85A97119AD09/Documents/apps/MY-NODE-APP07ACB03B-02AD-461C-86D5-48A274BFBC7E/src",
+  "author": {
+    "name": "Codeway"
+  },
+  "bugs": {
+    "url": "https://github.com/leblanmonster/sudokuresolver/issues"
+  },
+  "bundleDependencies": false,
+  "deprecated": false,
+  "description": "a sudoku resolver testing NOT all the position",
+  "homepage": "https://github.com/leblanmonster/sudokuresolver#readme",
+  "keywords": [
+    "sudoku",
+    "resolver"
+  ],
+  "license": "ISC",
+  "main": "index.js",
+  "name": "sudokuresolver",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/leblanmonster/sudokuresolver.git"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "version": "2.0.1"
+}
